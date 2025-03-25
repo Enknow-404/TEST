18a48a0c04281781e22b35d978b0739aa263bd1c
MOHAMED-FD <161535840+Enknow-404@users.noreply.github.com>
2025-03-25 07:17:05 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,11 @@
 import os, platform, time, sys
-
-print(' [+] Chekking Comand update')
-print('\033[1;32m') 
-os.system('  git pull')
 ranaxd = platform.architecture()[0]
 if ranaxd == '64bit':
- print(' [+] Your Device is 64bit')
+ print(' [+] Your Device is 64bit ...')
+ time.sleep(1)
  import M64
 elif ranaxd == '32bit':
- print(' [+] Your Devive is 32bit')
+ print(' [+] Your Devive is 32bit ...')
+ time.sleep(1)
  import M64
  
