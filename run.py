8b5c258d7137212cc2d0fbc94f751f9578cd8749
MOHAMED-FD <161535840+Enknow-404@users.noreply.github.com>
2025-03-25 07:19:28 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 import os, platform, time, sys
+
+
 ranaxd = platform.architecture()[0]
 if ranaxd == '64bit':
+ 	os.system('clear')	
  print(' [+] Your Device is 64bit ...')
  time.sleep(1)
  import M64
 elif ranaxd == '32bit':
+	os.system('clear')	
  print(' [+] Your Devive is 32bit ...')
  time.sleep(1)
  import M64
